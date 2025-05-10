788f2f6dc33b9efc0530097efa08d0c95bae6015
Yousef <yousef.baitalmal@gmail.com>
2025-05-11 00:27:43 +0300

    [CHECKPOINT]: in the process of refactoring

--- src/jsMain/kotlin/code/yousef/summon/runtime/PlatformRendererProvider.kt
+++
@@ -1,8 +0,0 @@
-package code.yousef.summon.runtime
-
-/**
- * JS implementation of the PlatformRenderer access functions.
- */
-
-// Note: This is intentionally kept minimal as the implementation is in the common code
-// and we just need to ensure the JS target compiles correctly. 
--- src/jvmMain/kotlin/code/yousef/summon/runtime/PlatformRendererProvider.kt
+++
@@ -1,8 +0,0 @@
-package code.yousef.summon.runtime
-
-/**
- * JVM implementation of the PlatformRenderer access functions.
- */
-
-// Note: This is intentionally kept minimal as the implementation is in the common code
-// and we just need to ensure the JVM target compiles correctly. 
